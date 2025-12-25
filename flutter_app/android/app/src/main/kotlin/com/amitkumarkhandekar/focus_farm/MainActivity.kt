4f5fac62c90d63deed5ea2c5757eb9324152b08b
amitkumarkhandekar <amitkumarkhandekar08@gmail.com>
2025-12-25 12:07:41 +0530

    Removed Flutter code

--- flutter_app/android/app/src/main/kotlin/com/amitkumarkhandekar/focus_farm/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.amitkumarkhandekar.focus_farm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('FocusFarm app smoke test', (WidgetTester tester) async {
-    // Basic smoke test placeholder
-    // Full widget tests require mocking Supabase and SharedPreferences
-    expect(true, isTrue);
-  });
-}
